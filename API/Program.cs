9dbfbb12ad47c6d9de357f2bdd0df6eec2aaaf32
sujeettng <sujeet_tng@hotmail.com>
2026-04-08 02:27:44 +0530

    Used Signal and promises to fetch data from APIs. Also resolved CORS

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 app.MapControllers();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.Run();
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+  protected readonly title = "Dating App";
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error) {
+      console.error('Error fetching members:', error);
+      throw error;
+    }
+  }
+
+
+
 }
